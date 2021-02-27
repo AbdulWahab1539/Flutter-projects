35bbe1532a86bc8b677aac72ea6de4d0d27283ad
Abdul Wahab <57368637+AbdulWahab1539@users.noreply.github.com>
2021-02-27 07:59:26 +0500

    Flash Chat - A Messaging App
    Flash chat is an messaging app built with integration of Google's firebase system. In which different can communicate with each other through text messages.

---
+++ flash-chat-flutter/lib/screens/login_screen.dart
@@ -0,0 +1,92 @@
+import 'package:flash_chat/constants.dart';
+import 'package:flash_chat/components/rounded_screen.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+
+class LoginScreen extends StatefulWidget {
+  static const String id = 'login_screen';
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  String email;
+  String password;
+  final _auth = FirebaseAuth.instance;
+  bool showSpinner = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Flexible(
+                child: Hero(
+                  tag: 'logo',
+                  child: Container(
+                    height: 200.0,
+                    child: Image.asset('images/logo.png'),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your email'),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                obscureText: true,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your password'),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                  colour: Colors.lightBlueAccent,
+                  buttonText: 'Log In',
+                  onPressed: () async{
+                    setState(() {
+                      showSpinner = true;
+                    });
+                    try{
+                      final userLoggedIn = await _auth.signInWithEmailAndPassword(email: email, password: password);
+                      if(userLoggedIn !=null ){
+                        Navigator.pushNamed(context, ChatScreen.id);
+                      }
+                      setState(() {
+                        showSpinner = false;
+                      });
+                    }catch(e){
+                      print(e);
+                    }
+
+                  })
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
